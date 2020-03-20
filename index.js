b211db9c9f231a1bf202963c7be50ff025bd4545
root <you@example.com>
2020-03-20 10:38:00 +0000

    l07

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
-const cool = require("cool-ascii-faces");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 var app = express();
 
-app.get("/cool",(request,response) => {
-	response.send("<html>"+cool()+"</html>")
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 80;
+
+var contacts = [
+	{ 
+		name: "peter",
+		phone: 123456	
+	},
+	{ 
+		name: "pablo",
+		phone: 789456	
+	}
+];
+
+const BASE_API_URL = "/api/v1";
+
+//GET CONTACTS
+
+app.get(BASE_API_URL+"/contacts", (req,res) =>{
+	res.send(JSON.stringify(contacts,null,2));
 });
 
-app.listen (80);
+//POST CONTACTS
 
-console.log("Server Ready");
+app.post(BASE_API_URL+"/contacts",(req,res) =>{
+	var newContact = req.body;
+	
+	if((newContact =="") || (newContact.name==null)){
+		res.sendStatus(400,"BAD REQUEST");
+	}else{
+		contacts.push(newContact); 
+		res.sendStatus(201,"CREATED");
+	}
+	
+});
+
+//DELETE CONTACTS
+
+//GET CONTACTS/xxx
+
+//PUT CONTACTS/xxx
+
+//DELETE CONTACTS/xxx
+
+app.listen(port, () => {
+	console.log("Server ready");
+});
+
+console.log("Starting server...");
